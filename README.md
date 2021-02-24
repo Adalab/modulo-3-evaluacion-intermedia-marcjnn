6cb94b63764441ec5ff6b4389751db55934afa9f
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-02-24 09:00:26 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# modulo-3-evaluacion-intermedia-marcjnn
+
+Ejercicio de evaluación intermedia en modulo 3 (React) en Adalab
---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import "../style/App.scss";
+
+import React from "react";
+
+// import data from "../data/data.json";
+
+class App extends React.Component {
+  render() {
+    return (
+      <p className="quote">
+        A mouse took a stroll throught the deep dark wood...
+      </p>
+    );
+  }
+}
+export default App;
