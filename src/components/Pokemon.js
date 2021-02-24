dd2442000fdae412701e376ad9807e330f280620
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-02-24 10:50:41 +0100

    add different background color depending on a pokemon type

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,11 +1,24 @@
 import "../style/components/Pokemon.scss";
 
 const Pokemon = (props) => {
+  let bgr = "";
+  if (props.pokemon.types.includes("grass")) {
+    bgr = "grass";
+  } else if (props.pokemon.types.includes("fire")) {
+    bgr = "fire";
+  } else if (props.pokemon.types.includes("water")) {
+    bgr = "water";
+  } else {bgr = "default"}
+
   const pokeTypes = props.pokemon.types.map((type, index) => {
-    return <li key={index} className="pokemon__item">{type}</li>;
+    return (
+      <li key={index} className="pokemon__item">
+        {type}
+      </li>
+    );
   });
   return (
-    <article className="pokemon">
+    <article className={`pokemon bgr--${bgr}`}>
       <img
         className="pokemon__img"
         src={props.pokemon.url}
